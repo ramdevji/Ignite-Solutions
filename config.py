6ac5b642f0a1e02e97959cfbc5e45c1e1079620c
RG <ravikishan@Ravis-MacBook-Air.local>
2025-07-12 20:14:21 +0530

    bug fixes for download

--- config.py
+++ config.py
@@ -2,8 +2,7 @@ import os
 
 class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get(
-        'DATABASE_URL',
-        'postgresql://ramdev:ramdev@localhost:5432/ignite_solutions'
+        'DATABASE_URL'
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG = True
